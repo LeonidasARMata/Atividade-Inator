166ee1ea92909d276fc4e2b002762543350a80ed
LeonidasARMata <leonidas.armata@gmail.com>
2026-04-05 08:25:22 -0300

    fixes

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-   API_URL: 'https://atividator.onrender.com',
+   API_URL: 'https://atividator2000.onrender.com',
   // Em desenvolvimento, descomente:
   // API_URL: 'http://localhost:3001',
 };
