449edab8643ac6cba9b5813f3ffac3f0926fd2ea
LeonidasARMata <leonidas.armata@gmail.com>
2026-04-06 21:30:04 -0300

    FIX

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-   API_URL: 'https://atividade-inator.onrender.com',
+   API_URL: 'https://atividade-inator.onrender.com/',
   // Em desenvolvimento, descomente:
   // API_URL: 'http://localhost:3001',
 };
